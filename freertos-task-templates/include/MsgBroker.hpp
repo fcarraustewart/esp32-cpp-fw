1b5b2059d42251e7494249bcb0a55541a1f350ca
fcarraustewart <39782345+fcarraustewart@users.noreply.github.com>
2022-05-21 12:29:02 -0300

    MsgBroker.hpp

---
+++ freertos-task-templates/include/MsgBroker.hpp
@@ -0,0 +1,35 @@
+#ifndef MSGBROKER__H_H
+#define MSGBROKER__H_H
+
+#include "freertos/FreeRTOS.h"
+#include "freertos/queue.h"
+#include "freertos/task.h"
+#include <string>
+
+namespace RTOS
+{
+    class MsgBroker
+    {
+    public:
+
+
+
+    private:
+
+
+
+
+
+    protected:
+        MsgBroker(){};
+
+    /**
+     *                  Member Variables:
+     */
+    public:
+
+    protected:
+    };
+
+}
+#endif
