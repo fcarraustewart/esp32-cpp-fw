065a42952b9fdfe14d888db19726e349666a715f
fcarraustewart <39782345+fcarraustewart@users.noreply.github.com>
2022-06-07 23:28:10 -0300

    Apply Prettier vsCode Extension.

--- freertos-task-templates/src/Blink.cpp
+++ freertos-task-templates/src/Blink.cpp
@@ -3,7 +3,6 @@
  * esp32 wrover kit tests.
  */
 
-
 #include <Arduino.h>
 #include "Services/LoRa.hpp"
 #include "Services/BLE.hpp"
@@ -15,83 +14,83 @@
 
 static RTOS::MsgBroker M = RTOS::MsgBroker();
 static volatile uint16_t interruptCounter;
-static hw_timer_t * timer = NULL;
+static hw_timer_t *timer = NULL;
 
 static const uint16_t msg = 0xAAAA;
 static const RTOS::MsgBroker::Message msgEE = {
-      .mSource =        0,
-      .mDestination =   1,
-      .mEvent =         RTOS::MsgBroker::Event::BLEConnected,
-      .mLength =        1,
-      .mPayload =       {0xEE,0xEE,0xEE,0xEE,0xEE,0xEE,0xEE,0xEE,0xEE,0xEE},
+    .mSource = 0,
+    .mDestination = 1,
+    .mEvent = RTOS::MsgBroker::Event::BLEConnected,
+    .mLength = 1,
+    .mPayload = {0xEE, 0xEE, 0xEE, 0xEE, 0xEE, 0xEE, 0xEE, 0xEE, 0xEE, 0xEE},
 };
 
-void IRAM_ATTR onTimer() {
-  Service::BLE::Send((uint8_t*)&(++interruptCounter));  /**< This runs the xQueueFromISR version of Send. */
-  //M << msgEE;
+void IRAM_ATTR onTimer()
+{
+  Service::BLE::Send((uint8_t *)&(++interruptCounter)); /**< This runs the xQueueFromISR version of Send. */
+  // M << msgEE;
 }
 
-template<class... Ts> 
-struct overload : Ts... 
-{ 
-  using Ts::operator()...; 
+template <class... Ts>
+struct overload : Ts...
+{
+  using Ts::operator()...;
 };
-template<class... Ts> 
+template <class... Ts>
 overload(Ts...) -> overload<Ts...>;
 
 void CreationOfVariantVectorWithServices()
 {
   /**
-   * @brief 
-   * 
+   * @brief
+   *
    * Variants templates indicate types that the variable variant can take.
    * Overload indicates the lambda done as operator() to each of the types you are interested in.
    * visit uses the operator() on the correct type that was loaded to the variant at the time visit is used.
-   * 
-   * 
+   *
+   *
    */
-    std::vector<  std::variant<   Service::BLE,   Service::LoRa   >>      // 1
-                  vecVariant = {  Service::BLE{}, Service::LoRa{} };
-  
-    for (auto& v: vecVariant){
-      
-        std::visit( overload  { 
-                      /* 
-                        One of these lambdas will be called for each type in vecVariant. 
-                      */
-                      [](const Service::BLE& x)         { 
-                                                          Logger::Log(  "Initializing: %s", x.mName.c_str());   
-                                                          x.Create(); 
-                                                        },
-                      [](const Service::LoRa& x)        { 
-                                                          Logger::Log(  "Initializing: %s", x.mName.c_str());   
-                                                          x.Create(); 
-                                                        }
-                    }, 
-                      /* 
-                        The element of the vecVariant to which the lambda will be applied. 
-                      */
-                    v 
-                  
-                  ); 
+  std::vector<std::variant<Service::BLE, Service::LoRa>> // 1
+      vecVariant = {Service::BLE{}, Service::LoRa{}};
 
-    }
-  
+  for (auto &v : vecVariant)
+  {
+
+    std::visit(overload{/*
+                          One of these lambdas will be called for each type in vecVariant.
+                        */
+                        [](const Service::BLE &x)
+                        {
+                          Logger::Log("Initializing: %s", x.mName.c_str());
+                          x.Create();
+                        },
+                        [](const Service::LoRa &x)
+                        {
+                          Logger::Log("Initializing: %s", x.mName.c_str());
+                          x.Create();
+                        }},
+               /*
+                 The element of the vecVariant to which the lambda will be applied.
+               */
+               v
+
+    );
+  }
 }
 
-void setup() {
+void setup()
+{
   CreationOfVariantVectorWithServices();
   timer = timerBegin(0, 2, true);
   timerAttachInterrupt(timer, &onTimer, true);
   timerAlarmWrite(timer, 1000000, true);
   timerAlarmEnable(timer);
 }
-             
+
 void loop()
 {
   delay(400);
-  Service::BLE::Send((uint8_t*)&msg);  
-  Service::LoRa::Send((uint8_t*)&msg);  
+  Service::BLE::Send((uint8_t *)&msg);
+  Service::LoRa::Send((uint8_t *)&msg);
   delay(400);
 }
- 
