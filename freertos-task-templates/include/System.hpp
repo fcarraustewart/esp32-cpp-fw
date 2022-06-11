348a16031ca38ce9ea4024cd26823e6a1f96d8e9
fcarraustewart <carraufelipe@gmail.com>
2022-06-11 08:45:51 -0300

    Create System and Utils/overload.hpp

---
+++ freertos-task-templates/include/System.hpp
@@ -0,0 +1,52 @@
+#ifndef __SYSTEM__H
+#define __SYSTEM__H
+#include <variant>
+#include <vector>
+#include "Services/LoRa.hpp"
+#include "Services/BLE.hpp"
+#include "Utils/overload.hpp"
+
+class System {
+#define _REGISTERED_SERVICES    Service::BLE, Service::LoRa
+#define REGISTERED_SERVICES     Service::BLE{}, Service::LoRa{}
+public:
+    static void Create()
+    {            
+            /**
+             * @brief
+             *
+             * Variants templates indicate types that the variable variant can take.
+             * Overload indicates the lambda done as operator() to each of the types you are interested in.
+             * visit uses the operator() on the correct type that was loaded to the variant at the time visit is used.
+             *
+             *
+             */
+            std::vector<std::variant<_REGISTERED_SERVICES>> mSystemServicesRegistered = {REGISTERED_SERVICES};
+
+            for (auto &v : mSystemServicesRegistered)
+            {
+
+                std::visit(overload{/*
+                                    One of these lambdas will be called for each type in mSystemServicesRegistered.
+                                    */
+                                    [](const Service::BLE &x)
+                                    {
+                                    Logger::Log("Initializing: %s", x.mName.c_str());
+                                    x.Create();
+                                    },
+                                    [](const Service::LoRa &x)
+                                    {
+                                    Logger::Log("Initializing: %s", x.mName.c_str());
+                                    x.Create();
+                                    }},
+                        /*
+                            The element of the mSystemServicesRegistered to which the lambda will be applied.
+                        */
+                        v
+
+                );
+            }
+    }
+};
+
+#endif
