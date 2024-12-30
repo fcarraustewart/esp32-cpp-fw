d3759ef53c0528997902bf2a58d01418857de18a
fcarraustewart <carraufelipe@gmail.com>
2024-12-30 09:46:10 +0100

    One way to do BLE extended advertising in the future.

--- zephyr-task-templates/include/Drivers/ble_state_machine.h
+++ zephyr-task-templates/include/Drivers/ble_state_machine.h
@@ -1,6 +1,12 @@
 #ifndef __BLE_STATE_MACHINE__H__
 #define __BLE_STATE_MACHINE__H__
 
+#ifdef __cplusplus
+extern "C" {
+#endif
 int ble_state_machine_run(void);
+#ifdef __cplusplus
+}
+#endif
 
 #endif
